c3afa6c8e4f3e956eb4be8c6ecb535b2da0d7897
Iryna Husieva <02guseva02@gmail.com>
2023-05-15 14:52:38 +0200

    sum

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,44 +1,17 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const gallery = document.querySelector('.gallery');
-const galleryElements = [];
-
-galleryItems.forEach(galleryItem => {
-
-    const item = document.createElement('li');
-    const link = document.createElement('a');
-    const image = document.createElement('img');
+const galleryElements = galleryItems.map(({ original, preview, description }) => {
+    const html = `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </li>
+    `;
     
-    item.classList.add('gallery__item');
-    link.classList.add('gallery__link');
-    image.classList.add('gallery__image');
-    
-    link.href = galleryItem.original;
-    image.src = galleryItem.preview;
-    image.alt = galleryItem.description;
-    
-    image.dataset.source = galleryItem.original;
-
-    item.append(link);
-    link.append(image);
-
-    galleryElements.push(item);
-
+    gallery.insertAdjacentHTML('beforeend', html);
 });
-gallery.append(...galleryElements);
-
-// const galleryItems = images.map(galleryItem => `<div class="gallery__item">
-//   <a class="gallery__link" href="${galleryItem.original}">
-//     <img
-//       class="gallery__image"
-//       src="${galleryItem.preview}"
-//       data-source="${galleryItem.original}"
-//       alt="${galleryItem.description}"
-//     />
-//   </a>
-// </div>`).join('');
-// createImg.insertAdjacentHTML('beforeend', galleryItems);
 
 // ---------------------------------------------------
 
@@ -63,16 +36,11 @@ function selectImg(event) {
             },
         });
     instance.show();
-    
         
-        window.addEventListener('keydown', (event) => {
-            if (event.code === 'Escape') {
+        function onPressEscKey(event) {
+            if (event.code == 'Escape') {
                 instance.close();
             }
-        });
-        function onPressEscKey(event) {
-            if (event.code !== 'Escape') return;
-            instance.close();
         }
     
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,32 +1,17 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
 const gallery = document.querySelector('.gallery');
-const galleryElements = [];
-
-galleryItems.forEach(galleryItem => {
-
-    const item = document.createElement('li');
-    const link = document.createElement('a');
-    const image = document.createElement('img');
-
+const galleryElements = galleryItems.map(({ original, preview, description }) => {
+    const html = `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </li>
+    `;
     
-    item.classList.add('gallery__item');
-    link.classList.add('gallery__link');
-    image.classList.add('gallery__image');
-    
-    link.href = galleryItem.original;
-    image.src = galleryItem.preview;
-    image.alt = galleryItem.description;
-
-    item.append(link);
-    link.append(image);
-
-    galleryElements.push(item);
-
+    gallery.insertAdjacentHTML('beforeend', html);
 });
-gallery.append(...galleryElements);
-
-console.log(galleryElements);
 
 // -------------------------------------------
 const lightbox = new SimpleLightbox('.gallery a', {
